4950374286887573dd6f7f01d5db3036be3039e6
root <root@ip-172-31-3-143.us-west-2.compute.internal>
2018-02-08 19:39:09 +0000

    Edited run.sh file

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 date
-/ebs2/spark/bin/spark-submit --master local[2] target/scala-2.11/simple-project_2.11-1.0.jar | tee logger.txt
+/ebs2/spark/bin/spark-submit --master local[1] target/scala-2.11/simple-project_2.11-1.0.jar |& tee logger.txt
 date
