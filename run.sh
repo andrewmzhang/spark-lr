5bd3f77f9262431ffdcc78ca8e01e8baa88fb74d
root <root@ip-172-31-3-143.us-west-2.compute.internal>
2018-02-09 05:57:04 +0000

    Added coalesce and set minpartitions to 1

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 date
-/ebs2/spark/bin/spark-submit --master local[1] target/scala-2.11/simple-project_2.11-1.0.jar |& tee logger.txt
+/ebs2/spark/bin/spark-submit --conf spark.ui.port=4041 --master local[1] target/scala-2.11/simple-project_2.11-1.0.jar |& tee logger.txt
 date
